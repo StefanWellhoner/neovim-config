5abfcf6d0704fc8a35090a6323ed58f40c66861d
Stefan Wellhoner <stefanwellhoner@ymail.com>
2024-07-29 15:15:08 +0200

    Initial Commit

---
+++ lua/stefan/plugins/colors.lua
@@ -0,0 +1,20 @@
+function ColorMyPencils(color)
+	color = color or "rose-pine-moon"
+	vim.cmd.colorscheme(color)
+end
+
+
+return {
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require('rose-pine').setup({
+				styles = {
+					italic = false,
+				}
+			})
+			ColorMyPencils()
+		end
+	},
+}
